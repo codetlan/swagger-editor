2a2821271a7c70a36f3057470dcf02230ff5f9b3
Emiliano Cabrera <jemiliano.cabrera@gmail.com>
2014-11-19 00:07:32 -0600

    Added sessions

--- app/scripts/enums/apigee-127.js
+++ app/scripts/enums/apigee-127.js
@@ -1,57 +1,53 @@
 'use strict';
 
 PhonicsApp.config(function ($provide) {
-  $provide.constant('defaults',
+    $provide.constant('defaults',
+                      
+                      // BEGIN-DEFAULTS-JSON
+                      {
+                          codegen: {
+                              servers: 'http://generator.wordnik.com/online/api/gen/servers',
+                              clients: 'http://generator.wordnik.com/online/api/gen/clients',
+                              server: 'http://generator.wordnik.com/online/api/gen/servers/{language}',
+                              client: 'http://generator.wordnik.com/online/api/gen/clients/{language}'
+                          },
+                          disableCodeGen: true,
 
-  // BEGIN-DEFAULTS-JSON
-  {
-    codegen: {
-      servers: 'http://generator.wordnik.com/online/api/gen/servers',
-      clients: 'http://generator.wordnik.com/online/api/gen/clients',
-      server: 'http://generator.wordnik.com/online/api/gen/servers/{language}',
-      client: 'http://generator.wordnik.com/online/api/gen/clients/{language}'
-    },
-    disableCodeGen: true,
+                          examplesFolder: '/spec-files/',
+                          exampleFiles: [
+                              'sessions.yaml'
+                          ],
 
-    examplesFolder: '/spec-files/',
-    exampleFiles: [
-      'default.yaml',
-      'minimal.yaml',
-      'heroku-pets.yaml',
-      'petstore.yaml',
-      'security.yaml'
-    ],
+                          autocompleteExtension: {
+                              'x-a127-config:': Object,
+                              'x-volos-resources': Object,
+                              paths: {
 
-    autocompleteExtension: {
-      'x-a127-config:': Object,
-      'x-volos-resources': Object,
-      paths: {
+                                  //path
+                                  '.': {
 
-      //path
-        '.': {
+                                      // operation
+                                      '.': {
+                                          'x-swagger-router-controller': String,
+                                          'x-volos-authorizations': Object,
+                                          'x-volos-apply': Object
+                                      }
+                                  }
+                              }
+                          },
 
-          // operation
-          '.': {
-            'x-swagger-router-controller': String,
-            'x-volos-authorizations': Object,
-            'x-volos-apply': Object
-          }
-        }
-      }
-    },
+                          backendEndpoint: '/editor/spec',
+                          useBackendForStorage: true,
+                          backendHelathCheckTimeout: 5000,
+                          useYamlBackend: true,
 
-    backendEndpoint: '/editor/spec',
-    useBackendForStorage: true,
-    backendHelathCheckTimeout: 5000,
-    useYamlBackend: true,
+                          disableFileMenu: true,
+                          headerBranding: true,
+                          enableTryIt: false,
+                          brandingCssClass: 'apigee-127',
+                          schemaUrl: '/schema/swagger.json'
+                      }
+                      // END-DEFAULTS-JSON
 
-    disableFileMenu: true,
-    headerBranding: true,
-    enableTryIt: false,
-    brandingCssClass: 'apigee-127',
-    schemaUrl: '/schema/swagger.json'
-  }
-  // END-DEFAULTS-JSON
-
-  );
+                     );
 });
--- app/scripts/enums/defaults.js
+++ app/scripts/enums/defaults.js
@@ -1,42 +1,38 @@
 'use strict';
 
 PhonicsApp.config(function ($provide) {
-  $provide.constant('defaults',
+    $provide.constant('defaults',
 
-  // BEGIN-DEFAULTS-JSON
-  {
-    codegen: {
-      servers: 'http://generator.wordnik.com/online/api/gen/servers',
-      clients: 'http://generator.wordnik.com/online/api/gen/clients',
-      server: 'http://generator.wordnik.com/online/api/gen/servers/{language}',
-      client: 'http://generator.wordnik.com/online/api/gen/clients/{language}'
-    },
-    disableCodeGen: true,
+                      // BEGIN-DEFAULTS-JSON
+                      {
+                          codegen: {
+                              servers: 'http://generator.wordnik.com/online/api/gen/servers',
+                              clients: 'http://generator.wordnik.com/online/api/gen/clients',
+                              server: 'http://generator.wordnik.com/online/api/gen/servers/{language}',
+                              client: 'http://generator.wordnik.com/online/api/gen/clients/{language}'
+                          },
+                          disableCodeGen: true,
 
-    examplesFolder: '/spec-files/',
-    exampleFiles: [
-      'default.yaml',
-      'minimal.yaml',
-      'heroku-pets.yaml',
-      'petstore.yaml',
-      'security.yaml'
-    ],
+                          examplesFolder: '/spec-files/',
+                          exampleFiles: [
+                              'sessions.yaml'
+                          ],
+                          
+                          // See keyword-map.js for object format
+                          autocompleteExtension: {},
 
-    // See keyword-map.js for object format
-    autocompleteExtension: {},
+                          backendEndpoint: '/editor/spec',
+                          useBackendForStorage: false,
+                          backendHelathCheckTimeout: 5000,
+                          useYamlBackend: false,
 
-    backendEndpoint: '/editor/spec',
-    useBackendForStorage: false,
-    backendHelathCheckTimeout: 5000,
-    useYamlBackend: false,
+                          disableFileMenu: false,
+                          headerBranding: false,
+                          enableTryIt: true,
+                          brandingCssClass: '',
+                          schemaUrl: '/schema/swagger.json'
+                      }
+                      // END-DEFAULTS-JSON
 
-    disableFileMenu: false,
-    headerBranding: false,
-    enableTryIt: true,
-    brandingCssClass: '',
-    schemaUrl: '/schema/swagger.json'
-  }
-  // END-DEFAULTS-JSON
-
-  );
+                     );
 });
